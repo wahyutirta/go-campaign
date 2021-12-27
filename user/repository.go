1721401f1b49ec350653797c09f82c382dcb073e
molent4 <57343997+molent4@users.noreply.github.com>
2021-12-27 21:56:36 +0800

    push initial project

---
+++ user/repository.go
@@ -0,0 +1,23 @@
+package user
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	Create(user User) (User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) Create(user User) (User, error) {
+	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, nil
+	}
+	return user, nil
+}
