d67b88ee476c3b41c90e7056a9135a1a12efe95b
molent4 <57343997+molent4@users.noreply.github.com>
2022-02-16 12:51:13 +0800

    feat: add transaction progress

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,7 +1,46 @@
 package handler
 
+import (
+	"gocampaign/helper"
+	"gocampaign/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 //parameter di uri
 // tangkap parameter, mapping ke input struct
 // panggil service, input struct sebagai parameter
 // service, dengan campaign id, bisa panggil repo
 // repo mencari data transaction suatu campaign
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+	err := c.ShouldBind(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Failed to bind campaign's id transactions", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	transactions, err := h.service.GetTransactionByCampaignID(input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Campaign Transactions", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
