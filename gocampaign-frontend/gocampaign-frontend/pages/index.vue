53e7aed3921a4e8f45cc4febb5ef00d8d4b3147e
molent4 <57343997+molent4@users.noreply.github.com>
2022-03-07 23:40:28 +0800

    add login page

--- gocampaign-frontend/gocampaign-frontend/pages/index.vue
+++ gocampaign-frontend/gocampaign-frontend/pages/index.vue
@@ -250,12 +250,12 @@
                 <div>45%</div>
                 <div class="ml-auto font-semibold">Rp 55.000.000</div>
               </div>
-              <a
-                href="/projects.html"
+              <nuxt-link
+                to="/projects/1"
                 class="text-center mt-5 button-cta block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-2 text-lg rounded-full"
               >
                 Fund Now
-              </a>
+              </nuxt-link>
             </div>
           </div>
         </div>
@@ -291,12 +291,12 @@
                 <div>70%</div>
                 <div class="ml-auto font-semibold">Rp 75.000.000</div>
               </div>
-              <a
-                href="/projects.html"
+              <nuxt-link
+                to="/projects/1"
                 class="text-center mt-5 button-cta block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-2 text-lg rounded-full"
               >
                 Fund Now
-              </a>
+              </nuxt-link>
             </div>
           </div>
         </div>
@@ -332,12 +332,12 @@
                 <div>10%</div>
                 <div class="ml-auto font-semibold">Rp 35.000.000</div>
               </div>
-              <a
-                href="/projects.html"
+              <nuxt-link
+                to="/projects/1"
                 class="text-center mt-5 button-cta block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-2 text-lg rounded-full"
               >
                 Fund Now
-              </a>
+              </nuxt-link>
             </div>
           </div>
         </div>
