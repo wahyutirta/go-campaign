3c85666116e586adb8d758427788c9094e76a9ad
molent4 <57343997+molent4@users.noreply.github.com>
2022-06-15 10:08:06 +0800

    upload db

--- gocampaign.sql
+++ gocampaign.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2022 at 10:03 AM
+-- Generation Time: Jun 15, 2022 at 04:06 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.14
 
@@ -47,7 +47,7 @@ CREATE TABLE `campaigns` (
 --
 
 INSERT INTO `campaigns` (`id`, `user_id`, `name`, `short_description`, `description`, `perks`, `backer_count`, `goal_amount`, `current_amount`, `slug`, `created_at`, `updated_at`) VALUES
-(1, 1, 'campaign baru satu habis update', 'ini campaign baru banget', 'ini campaign baru banget fix sih ya gitu kata admin', 'satu perks, dua perks, tiga perks, empat perks', 0, 10000, 0, 'test-campaign', '2022-01-12 13:48:14', '2022-01-25 21:03:23'),
+(1, 1, 'post new campaign hacked', 'ini campaign baru banget', 'ini campaign baru banget fix sih ya gitu kata admin', 'satu perks, dua perks, tiga perks, empat perks', 1, 10000, 10000000, 'test-campaign', '2022-01-12 13:48:14', '2022-03-05 01:21:21'),
 (2, 1, 'Test Penggalangan Dana', 'short test', 'long test', 'satu, dua, tiga', 0, 10000, 0, 'test-penggalangan-dana-1', '2022-01-22 23:54:51', '2022-01-22 23:54:51'),
 (3, 1, 'campaign baru satu', 'ini campaign baru banget', 'ini campaign baru banget fix sih ya gitu kata admin', 'satu perks, dua perks, tiga perks', 0, 10000, 0, 'campaign-baru-satu-1', '2022-01-23 00:30:07', '2022-01-23 00:30:07'),
 (4, 1, 'campaign baru satu habis update', 'ini campaign baru banget', 'ini campaign baru banget fix sih ya gitu kata admin', 'satu perks, dua perks, tiga perks, empat perks', 0, 10000, 0, 'campaign-baru-satu-habis-update-1', '2022-01-25 21:02:24', '2022-01-25 21:02:24'),
@@ -90,6 +90,7 @@ CREATE TABLE `transactions` (
   `amount` int(11) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
   `code` varchar(255) DEFAULT NULL,
+  `payment_url` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -98,9 +99,22 @@ CREATE TABLE `transactions` (
 -- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`id`, `campaign_id`, `user_id`, `amount`, `status`, `code`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 10000, 'paid', NULL, '2022-02-16 07:39:57', '2022-02-16 07:39:57'),
-(2, 1, 2, 15000, 'paid', NULL, '2022-02-16 08:24:34', '2022-02-16 08:24:34');
+INSERT INTO `transactions` (`id`, `campaign_id`, `user_id`, `amount`, `status`, `code`, `payment_url`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 10000, 'paid', NULL, '', '2022-02-16 07:39:57', '2022-02-16 07:39:57'),
+(2, 1, 2, 15000, 'paid', NULL, '', '2022-02-16 08:24:34', '2022-02-16 08:24:34'),
+(3, 1, 1, 10000000, 'pending', '', '', '2022-03-03 12:36:27', '2022-03-03 12:36:27'),
+(4, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:24:36', '2022-03-03 18:24:37'),
+(5, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:25:49', '2022-03-03 18:25:50'),
+(6, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:28:47', '2022-03-03 18:28:48'),
+(7, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:31:03', '2022-03-03 18:31:04'),
+(8, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:31:42', '2022-03-03 18:31:42'),
+(9, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:37:49', '2022-03-03 18:37:51'),
+(10, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:38:16', '2022-03-03 18:38:17'),
+(11, 1, 1, 10000000, 'pending', '', 'payment-url', '2022-03-03 18:54:02', '2022-03-03 18:54:04'),
+(12, 1, 1, 10000000, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/19a57db7-bc87-4210-aa54-5c924b1e91c7', '2022-03-03 18:55:12', '2022-03-03 18:55:13'),
+(13, 1, 1, 10000000, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/d93ed5c0-529b-4251-adfd-ffd0f45d9789', '2022-03-03 19:02:13', '2022-03-03 19:02:14'),
+(14, 1, 1, 10000000, 'pending', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/59abe8ca-349f-4942-972e-ada4d166f91a', '2022-03-03 19:41:04', '2022-03-03 19:41:04'),
+(15, 1, 1, 10000000, 'paid', '', 'https://app.sandbox.midtrans.com/snap/v2/vtweb/6985f3d6-7a3a-443f-834d-00d7cc8bc143', '2022-03-03 19:46:35', '2022-03-05 01:21:21');
 
 -- --------------------------------------------------------
 
@@ -177,7 +191,7 @@ ALTER TABLE `campaign_images`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
